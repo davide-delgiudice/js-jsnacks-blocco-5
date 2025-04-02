adab238b30dc161330e15f9297b9d4eb13c82f45
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-02 16:00:01 +0200

    [UPD] used filter to log first new array

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,13 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const longZucchine = zucchine.filter((long) => {
+  if(long.length > 15){
+    return true;
+  }
+
+  return false;
+})
+
+console.log(longZucchine);
