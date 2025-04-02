f03847244a758056ae802e5dbc475c0b438d52ad
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-02 15:40:52 +0200

    [UPD] used map to log new array with authors - ex 04 Terminato

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const onlyName = posts.map((names) => {
+  return names.author;
+})
+
+console.log(onlyName);
